a9729fa50b3a797f60553ae1afb17d931fcb4bb1
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-01-30 15:13:27 -0700

    Standard Deviation commit

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -3,3 +3,19 @@ ages = [24, 30, 18, 20, 41]
 # Your code here for calculating the standard deviation
 
 # When you find the standard deviation, print it out
+
+total = ages.sum
+
+mean = total / ages.count.to_f
+
+subtract_mean = ages.map {|age| (age - mean).round(2)}
+
+squared = subtract_mean.map {|num| (num ** 2).round(2)}
+
+sum = squared.reduce(:+).round(2)
+
+mean2 = sum / ages.count
+
+standard_deviation = Math.sqrt(mean2).round(2)
+
+p standard_deviation
